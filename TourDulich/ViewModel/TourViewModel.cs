b35f61a94745aa37a89e420541a576d13c61aa1b
Dark Traceur <tandat27062000@gmail.com>
2021-11-05 21:20:49 +0700

    Doing - Reponsive

--- TourDulich/ViewModel/TourViewModel.cs
+++ TourDulich/ViewModel/TourViewModel.cs
@@ -17,7 +17,10 @@ namespace TourDulich.ViewModel
         public ICommand AddTour { get; set; }
         public ICommand EditTour { get; set; }
         public ICommand DeleteTour { get; set; }
-        
+        public ICommand CloseTourAdd { get; set; }
+        public ICommand CloseTourEdit { get; set; }
+        public ICommand CloseTourDelete { get; set; }
+     
         public TourViewModel()
         {
 
@@ -28,12 +31,18 @@ namespace TourDulich.ViewModel
             AddTour = new RelayCommand<object>(p => { return true; }, p => { Add(); });
             EditTour = new RelayCommand<object>(p => { return true; }, p => { Edit(); });
             DeleteTour = new RelayCommand<object>(p => { return true; }, p => { Delete(); });
+            CloseTourAdd = new RelayCommand<object>(p => { return true; }, p => { CloseAdd(p); });
+            CloseTourEdit = new RelayCommand<object>(p => { return true; }, p => { CloseEdit(p); });
+            CloseTourDelete = new RelayCommand<object>(p => { return true; }, p => { CloseDelete(p); });
 
             #endregion
         }
 
-        private void Add() { TourManagerAdd x = new TourManagerAdd(); x.ShowDialog(); }
+        private void Add() { TourManagerAdd x = new TourManagerAdd(); x.ShowDialog();}
         private void Edit() { TourManagerEdit x = new TourManagerEdit(); x.ShowDialog(); }
         private void Delete() { TourManagerDelete x = new TourManagerDelete(); x.ShowDialog(); }
+        private void CloseAdd(object obj) { TourManagerAdd x = obj as TourManagerAdd; x.Close(); }
+        private void CloseEdit(object obj) { TourManagerEdit x = obj as TourManagerEdit; x.Close(); }
+        private void CloseDelete(object obj) { TourManagerDelete x = obj as TourManagerDelete; x.Close(); }
     } 
 }
