3a42b7f2a9218abfec811ae7f4c3fe91fa94830d
Đạt Nhân Trương <truongdatnhan@gmail.com>
2021-11-09 01:44:58 +0700

    Add Default Models Back

---
+++ TourDulich/Model/TourGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourDulich.Model
+{
+    class TourGroup
+    {
+        public string TenDoan { get; set; }
+        public DateTime? NgayDi { get; set; }
+        public DateTime? NgayVe { get; set; }
+        public string DoanhThu { get; set; }
+        public string NoiDung { get; set; }
+        public string MaDoan { get; set; }
+        public List<NhanVien> NhanVien { get; set; }
+        public Tour Tour { get; set; }
+    }
+}
