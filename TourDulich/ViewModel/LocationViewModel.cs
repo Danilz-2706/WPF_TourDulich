4cff77e3a31847dcefba9323f05b3b754da85e4a
Đạt Nhân Trương <truongdatnhan@gmail.com>
2021-11-10 15:28:50 +0700

    Location,Type View and ViewModel chưa Binding

---
+++ TourDulich/ViewModel/LocationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourDulich.Model;
+
+namespace TourDulich.ViewModel
+{
+    public class LocationViewModel : BaseViewModel
+    {
+        public string MaDiaDiem { get; set; }
+        public string TenDiaDiem { get; set; }
+        public ObservableCollection<DiaDiemDTO> List { get; set; }
+
+    }
+}
---
+++ TourDulich/ViewModel/TypeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourDulich.Model;
+
+namespace TourDulich.ViewModel
+{
+    public class TypeViewModel : BaseViewModel
+    {
+        public string MaLoaiHinh { get; set; }
+        public string TenLoaiHinh { get; set; }
+        public ObservableCollection<LoaiHinhDuLichDTO> List { get; set; }
+    }
+}
