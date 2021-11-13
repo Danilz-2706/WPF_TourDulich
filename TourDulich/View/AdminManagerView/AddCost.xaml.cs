fade4cef858f17b989b64a0e288b5586fd07c580
ViLam <kulam1782000@gmail.com>
2021-11-12 19:18:44 -0800

    Lâm làm addCost nè

---
+++ TourDulich/View/AdminManagerView/AddCost.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TourDulich.View.AdminManagerView
+{
+    /// <summary>
+    /// Interaction logic for AddCost.xaml
+    /// </summary>
+    public partial class AddCost : Window
+    {
+        public AddCost()
+        {
+            InitializeComponent();
+        }
+    }
+}
