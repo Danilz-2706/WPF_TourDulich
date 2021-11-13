4fa2ab733c1f28ef275c67e6478d043dc0b17654
Đạt Nhân Trương <truongdatnhan@gmail.com>
2021-11-13 13:53:20 +0700

    Update Database

--- Domain/Entities/DoanDuLich.cs
+++ Domain/Entities/DoanDuLich.cs
@@ -13,9 +13,10 @@ namespace Domain.Entities
         public DateTime NgayKhoiHanh { get; set; }
         public DateTime NgayKetThuc { get; set; }
         public long DoanhThu { get; set; }
-
         public ICollection<ChiTietDoan> ChiTietDoans { get; set; }
+        public ICollection<Khach> Khaches { get; set; }
         public ICollection<ChiPhi> ChiPhis { get; set; }
         public ICollection<PhanBoNhanVienDoan> PhanBoNhanVienDoans { get; set; }
+        public ICollection<NhanVien> NhanViens { get; set; }
     }
 }
